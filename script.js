056519bc28efd5419bb34217b2f36cb6f0116cd3
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-03-23 00:24:55 +0600

    fhgdfhdf

--- script.js
+++ script.js
@@ -64,3 +64,54 @@ document.getElementById("button").addEventListener("click",
 //         document.getElementById("main-input").value= mainInput.value + buttonValue;
 //     }
 // });
+
+document.getElementById("button").addEventListener("click", 
+(event)=>{
+    const buttonValue = (event.target.innerText);
+    console.log(buttonValue);
+
+    const mainInput = document.getElementById("main-input");
+    // mainInput.value = buttonValue;
+
+    if (buttonValue == "AC") {
+        mainInput.value = "";
+    }
+    else if (buttonValue == "+/-") {
+        mainInput.value = `-${mainInput.value}`;
+    }
+    else if (buttonValue == "%") {
+        const inputValue = mainInput.value;
+        const regex = /(\d+)(%)/g;
+        const match = regex.exec(inputValue);
+        if (match !== null) {
+            const partialValue = parseFloat(match[1]);
+            const result = percentage(partialValue, 100);
+            mainInput.value = result;
+        }
+        else {
+            const inputValue = parseFloat(mainInput.value);
+            if (isNaN(inputValue)) {
+                mainInput.value = "Error";
+            }
+            else {
+                const result = inputValue / 100;
+                mainInput.value = result;
+            }
+        }
+    }
+    else if (buttonValue == "=") {
+        try {
+            mainInput.value = eval(mainInput.value);
+        }
+        catch (err) {
+            mainInput.value = "Error";
+        }
+    }
+    else{
+        document.getElementById("main-input").value= mainInput.value + buttonValue;
+    }
+});
+
+function percentage(partialValue, totalValue) {
+    return (partialValue * totalValue) / 100;
+}
