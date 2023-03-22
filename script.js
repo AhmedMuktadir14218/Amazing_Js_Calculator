201300ddb1c94a0f82bb26f15c38066f762edfa8
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-03-23 00:24:23 +0600

    fhgdfhdf

--- script.js
+++ script.js
@@ -27,3 +27,40 @@ document.getElementById("button").addEventListener("click",
         document.getElementById("main-input").value= mainInput.value + buttonValue;
     }
 })
+
+// document.getElementById("button").addEventListener("click", 
+// (event)=>{
+//     const buttonValue = (event.target.innerText);
+//     console.log(buttonValue);
+
+//     const mainInput = document.getElementById("main-input");
+//     // mainInput.value = buttonValue;
+
+//     if (buttonValue == "AC") {
+//         mainInput.value = "";
+//     }
+//     else if (buttonValue == "+/-") {
+//         mainInput.value = `-${mainInput.value}`;
+//     }
+//     else if (buttonValue == "%") {
+//         const inputValue = parseFloat(mainInput.value);
+//         if (isNaN(inputValue)) {
+//             mainInput.value = "Error";
+//         }
+//         else {
+//             const result = inputValue / 100;
+//             mainInput.value = result;
+//         }
+//     }
+//     else if (buttonValue == "=") {
+//         try {
+//             mainInput.value = eval(mainInput.value);
+//         }
+//         catch (err) {
+//             mainInput.value = "Error";
+//         }
+//     }
+//     else{
+//         document.getElementById("main-input").value= mainInput.value + buttonValue;
+//     }
+// });
