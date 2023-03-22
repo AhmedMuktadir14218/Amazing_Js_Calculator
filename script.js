e4fc594f84c206d3414a04e26d5e2e09494484ec
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-03-23 00:26:07 +0600

    h

--- script.js
+++ script.js
@@ -119,60 +119,60 @@ document.getElementById("button").addEventListener("click",
 
 
 
-document.getElementById("button").addEventListener("click", 
-(event)=>{
-    const buttonValue = (event.target.innerText);
-    console.log(buttonValue);
+// document.getElementById("button").addEventListener("click", 
+// (event)=>{
+//     const buttonValue = (event.target.innerText);
+//     console.log(buttonValue);
 
-    const mainInput = document.getElementById("main-input");
-    // mainInput.value = buttonValue;
+//     const mainInput = document.getElementById("main-input");
+//     // mainInput.value = buttonValue;
 
-    if (buttonValue == "AC") {
-        mainInput.value = "";
-    }
-    else if (buttonValue == "+/-") {
-        mainInput.value = `-${mainInput.value}`;
-    }
-    else if (buttonValue == "%") {
-        const inputValue = mainInput.value;
-        const regex = /(\d+)(%)/g;
-        const match = regex.exec(inputValue);
-        if (match !== null) {
-            const partialValue = parseFloat(match[1]);
-            const result = percentage(partialValue, 100);
-            mainInput.value = result;
-        }
-        else {
-            const inputValue = parseFloat(mainInput.value);
-            if (isNaN(inputValue)) {
-                mainInput.value = "Error";
-            }
-            else {
-                const result = inputValue / 100;
-                mainInput.value = result;
-            }
-        }
-    }
-    else if (buttonValue == "=") {
-        try {
-            mainInput.value = eval(mainInput.value);
-        }
-        catch (err) {
-            mainInput.value = "Error";
-        }
-    }
-    else{
-        document.getElementById("main-input").value= mainInput.value + buttonValue;
-    }
-});
+//     if (buttonValue == "AC") {
+//         mainInput.value = "";
+//     }
+//     else if (buttonValue == "+/-") {
+//         mainInput.value = `-${mainInput.value}`;
+//     }
+//     else if (buttonValue == "%") {
+//         const inputValue = mainInput.value;
+//         const regex = /(\d+)(%)/g;
+//         const match = regex.exec(inputValue);
+//         if (match !== null) {
+//             const partialValue = parseFloat(match[1]);
+//             const result = percentage(partialValue, 100);
+//             mainInput.value = result;
+//         }
+//         else {
+//             const inputValue = parseFloat(mainInput.value);
+//             if (isNaN(inputValue)) {
+//                 mainInput.value = "Error";
+//             }
+//             else {
+//                 const result = inputValue / 100;
+//                 mainInput.value = result;
+//             }
+//         }
+//     }
+//     else if (buttonValue == "=") {
+//         try {
+//             mainInput.value = eval(mainInput.value);
+//         }
+//         catch (err) {
+//             mainInput.value = "Error";
+//         }
+//     }
+//     else{
+//         document.getElementById("main-input").value= mainInput.value + buttonValue;
+//     }
+// });
 
-function percentage(partialValue, totalValue) {
-    const partialString = partialValue.toString();
-    if (partialString.endsWith("%")) {
-        partialValue = parseFloat(partialString.slice(0, -1));
-        return (partialValue * totalValue) / 100;
-    }
-    else {
-        return (partialValue * totalValue) / 100;
-    }
-}
+// function percentage(partialValue, totalValue) {
+//     const partialString = partialValue.toString();
+//     if (partialString.endsWith("%")) {
+//         partialValue = parseFloat(partialString.slice(0, -1));
+//         return (partialValue * totalValue) / 100;
+//     }
+//     else {
+//         return (partialValue * totalValue) / 100;
+//     }
+// }
